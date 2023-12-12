cb887d983f2be197ea1c6c01ab5edd73246d227a
ItsBorges <137112782+ItsBorges@users.noreply.github.com>
2023-12-12 19:15:19 -0300

    Add files via upload

---
+++ SOS_WebDev/Trabalho 2 Bimestre/Quiz/questoes.js
@@ -0,0 +1,29 @@
+export default[
+    {
+        question: "Quanto é 9+3x11?",
+        answers: [
+            {option: "42", correct: true },
+            {option: "132", correct: false},
+            {option: "28", correct: false},
+        ],
+    },
+    {
+        question: "Quanto é 4 elevado ao cubo?",
+        answers:[
+            { option: "64", correct: true},
+            { option: "70", correct: false},
+            { option: "54", correct: false},
+        ],
+    },
+        {
+            question: "Quanto é 200 x 8?",
+            answers:[
+                { option: "1900", correct: false},
+                { option: "1600", correct: true},
+                { option: "1000", correct: false},
+            ]
+        }
+    ]
+        
+    
+    
